c4eaff1b78f2617396ef0af81e01158b3d838901
JayBeezy <jakebees@ku.edu>
2022-07-24 03:46:34 -0500

    Adding custom domain name v2

---
+++ precache-manifest.f36bc01b1b5e7532fe6e0d3152327fd5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a7d86a48295a5421691",
+    "url": "/static/css/main.04f22c1a.chunk.css"
+  },
+  {
+    "revision": "5a7d86a48295a5421691",
+    "url": "/static/js/main.5a7d86a4.chunk.js"
+  },
+  {
+    "revision": "7d9504b57e8feadddb7b",
+    "url": "/static/css/1.84561cca.chunk.css"
+  },
+  {
+    "revision": "7d9504b57e8feadddb7b",
+    "url": "/static/js/1.7d9504b5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c82e021bd55bcd667894bfdcc95dc7e2",
+    "url": "/static/media/shrikhand-latin-400.c82e021b.woff2"
+  },
+  {
+    "revision": "9cb149a91dea4f7eb734ba3a29569de1",
+    "url": "/static/media/shrikhand-latin-400.9cb149a9.woff"
+  },
+  {
+    "revision": "f2de0cbee0d43bf943a03ae0a0aab9dc",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/DoughBoy/precache-manifest.cb021ad22aa4b455ed3f7f5718ec6617.js"
+  "/precache-manifest.f36bc01b1b5e7532fe6e0d3152327fd5.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/DoughBoy/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
