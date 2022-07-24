8fefd9efcef049250c0d46b1948df52be70fa4f1
JayBeezy <jakebees@ku.edu>
2022-07-24 01:46:15 -0500

    Updates

---
+++ precache-manifest.3db515c45a0ec9c45834c8610f3c82b7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef9238a7fbc4c9f9034d",
+    "url": "/DoughBoy/static/css/main.27a7b25c.chunk.css"
+  },
+  {
+    "revision": "ef9238a7fbc4c9f9034d",
+    "url": "/DoughBoy/static/js/main.ef9238a7.chunk.js"
+  },
+  {
+    "revision": "14e5fcc5003e7699ef99",
+    "url": "/DoughBoy/static/css/1.303e90b6.chunk.css"
+  },
+  {
+    "revision": "14e5fcc5003e7699ef99",
+    "url": "/DoughBoy/static/js/1.14e5fcc5.chunk.js"
+  },
+  {
+    "revision": "02aa271138ecb0dcd97b",
+    "url": "/DoughBoy/static/js/runtime~main.02aa2711.js"
+  },
+  {
+    "revision": "9cb149a91dea4f7eb734ba3a29569de1",
+    "url": "/DoughBoy/static/media/shrikhand-latin-400.9cb149a9.woff"
+  },
+  {
+    "revision": "c82e021bd55bcd667894bfdcc95dc7e2",
+    "url": "/DoughBoy/static/media/shrikhand-latin-400.c82e021b.woff2"
+  },
+  {
+    "revision": "9c878edbf580dc181a67c028c59a1235",
+    "url": "/DoughBoy/index.html"
+  }
+];
